863fe732c6e9cdf3462a490558a4e6e4c0571fd4
ry <renyanstone@163.com>
2019-05-27 15:28:19 +0800

    commit the task

--- main/main.js
+++ main/main.js
@@ -1,4 +1,28 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    var arr = num.split('');
+    var led = {
+        top :   ["._.","...","._.","._.","...","._.","._.","._.","._.","._."],
+        mid :  ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"],
+        down : ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]
+    }
+    var l = arr.length;
+    let t="";
+    let m="";
+    let d="";
+
+    for (var i=0; i<l; i++){
+        if (i<l-1){
+            t += led.top[arr[i]]+" ";
+            m += led.mid[arr[i]]+" ";
+            d += led.down[arr[i]]+" ";
+        }
+        else{
+            t += led.top[arr[i]]+"\n";
+            m += led.mid[arr[i]]+"\n";
+            d += led.down[arr[i]]+"\n";
+        }
+    }
+    let out=t+m+d;
+    return out;
+
 };
